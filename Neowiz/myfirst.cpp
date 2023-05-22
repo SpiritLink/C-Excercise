35f60bc96510c6a7f73e7794cff69325029b31b2
spiritlink <spiritlink@neowiz.com>
2023-05-22 09:40:05 +0900

    C++ 연습 프로젝트 초기 설정

---
+++ Neowiz/myfirst.cpp
@@ -0,0 +1,12 @@
+#include "myfirst.h"
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;                        // 정의 가시화 (std 사용 불필요)
+    cout << "C++의 세계로 오십시오.";
+    cout << endl;
+    cout << "후회하지 않으실 겁니다!" << endl;
+    return 0; // main 종료
+}
